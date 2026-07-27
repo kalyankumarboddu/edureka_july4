a82e8a5ca841419f830a440e50bfb4b841caab28
kalyankumarboddu <kalyankumarboddu@gmail.com>
2026-07-27 21:52:46 +0530

    push updated

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from dotenv import load_dotenv
 import os
-
+from groq import Groq
 ## add required file name in .gitignore such that i wont be imported to git repo, now added .env file in .gitignore
 load_dotenv()
-print("Key value for Key1 is", os.getenv("Key1"))
-print("Key value for Key2 is", os.getenv("KEY2"))
-print("Key value for No Key is", os.getenv("Key3"))
 
 
+def main():
+    while True:
+        input_text=input("Enter message (you):")
+        client = Groq(api_key=os.getenv("Gorq_Key"))
+        if input_text.lower() == "exit":
+            break
+        response = client.chat.completions.create(
+            model=os.getenv("model_name"),
+            messages=[{"role":"user", "content":input_text}],
+            temperature=0.2,
+            max_tokens=20
+        )
+        print("AI: ", response.choices[0].message.content)
 
 
-# def main():
-#     print("Hello from uv-project!")
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
